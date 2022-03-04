63ddae2917cdc2b544f07d275d9d58314ffde686
Eddsalasgit <edslgn.1234@gmail.com>
2022-03-03 18:55:28 -0600

    commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+const withAPI =require('next-pwa')
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withAPI ({
   reactStrictMode: true,
+  pwa:{
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV==='development'
+  },
   i18n:{
     locales: ["en", "es"],
     defaultLocale: "es"
   }
-}
+})
 
 module.exports = nextConfig
